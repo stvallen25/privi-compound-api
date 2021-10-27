f507b5e107f3946cb8e0a3e6bc05facbf516dc2d
stvallen25 <stvallen25@gmail.com>
2021-10-26 20:14:18 -0500

    update endpoint for proposals

--- src/controllers/CompoundController.ts
+++ src/controllers/CompoundController.ts
@@ -1,12 +1,23 @@
 /* eslint-disable prettier/prettier */
 import Compound from '@compound-finance/compound-js'
-import { CTokenServiceRequest } from '@compound-finance/compound-js/dist/nodejs/types'
+import { CTokenServiceRequest, GovernanceServiceRequest } from '@compound-finance/compound-js/dist/nodejs/types'
 import { JsonController, Get, QueryParams } from 'routing-controllers'
 
 @JsonController()
 export class CompoundController {
   @Get('/compound/ctoken')
   async getCtokenList(@QueryParams() query: CTokenServiceRequest) {
+    try {
+      const response = await Compound.api.cToken(query)
+      return response
+    } catch (e: any) {
+      console.log(e.message)
+      throw new Error(e.message)
+    }
+  }
+
+  @Get('/compound/proposals')
+  async getProposals(@QueryParams() query: GovernanceServiceRequest) {
     try {
       const response = await Compound.api.cToken(query)
       return response
